dcfb982dfe8afb18b5d0784a459b437d620e0ff7
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-24 14:33:34 +0900

    Model 추가 -v3

---
+++ servlet/src/main/java/min/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,9 @@
+package min.servlet.web.frontcontroller.v3;
+
+import min.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+    ModelView process(Map<String, String> paramMap);
+}
